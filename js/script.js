480c58b52c015dc3e5f105260d9a93a6ce2a8e10
GianluGiarde <gianlugiarde@gmail.com>
2023-04-14 01:49:38 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,30 +1,54 @@
-const userName = document.querySelector("#name").value;
-const distance = parseFloat(document.querySelector("#km").value);
-const userAge = parseInt(document.querySelector("#age").value);
-const pricePerKm = 0.21;
+const ticket = document.querySelector(".ticket").classList;
 
 // generator event
 function generate() {
-    let fullPrice = (distance * pricePerKm).toFixed(2);
-    let under18Price = (fullPrice * 0.6).toFixed(2);
-    let over65Price = (fullPrice * 0.8).toFixed(2);
+    const pricePerKm = 0.21;
+    const userName = document.getElementById("name").value;
+    const distance = parseFloat(document.getElementById("km").value);
+    const userAge = parseInt(document.getElementById("age").value);
+    const fullPrice = (distance * pricePerKm).toFixed(2);
+    const under18Price = (fullPrice * 0.6).toFixed(2);
+    const over65Price = (fullPrice * 0.8).toFixed(2);
 
-    if (userAge < 18) {
-        console.log(under18Price);
-    } else if (userAge >= 65) {
-        console.log(over65Price);
-    } else {
-        console.log(fullPrice);
+    if (!isNaN(distance) && !isNaN(userAge)) {
+        // userName
+        document.querySelector("#user_name").innerHTML = `${userName}`;
+
+        // offer & ticket_price
+        if (userAge < 18) {
+            document.querySelector("#offer").innerHTML = `Biglietto Under18`
+            document.querySelector("#ticket_price").innerHTML = `${under18Price}€`;
+        } else if (userAge >= 65) {
+            document.querySelector("#offer").innerHTML = `Biglietto Over65`
+            document.querySelector("#ticket_price").innerHTML = `${over65Price}€`;
+        } else {
+            document.querySelector("#offer").innerHTML = `Biglietto Standard`
+            document.querySelector("#ticket_price").innerHTML = `${fullPrice}€`;
+        }
+
+        // carriage
+        document.querySelector("#carriage").innerHTML = Math.floor(Math.random() * 10) + 1;
+
+        // code_cp
+        document.querySelector("#code_cp").innerHTML = Math.floor(Math.random() * 100000) + 1;
+
+        reset();
+        ticket.add("show");
     }
-
-    reset();
 };
 
 // reset
 function reset() {
-    document.querySelector("#name").value = "";
-    document.querySelector("#km").value = "";
-    document.querySelector("#age").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("km").value = "";
+    document.getElementById("age").value = "";
 }
 
+//fullReset
+function fullReset() {
+    ticket.remove("show");
+    document.getElementById("name").value = "";
+    document.getElementById("km").value = "";
+    document.getElementById("age").value = "";
+}
 
